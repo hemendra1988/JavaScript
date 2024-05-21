1532df8d28b9423ffac2b4f95a9eecf30f7e71eb
hemendra1988 <hemendravermaiiet@gmail.com>
2024-05-21 21:41:58 +0000

    updates

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all JS code as newer version?
+
+/* alert("Hello !!")
+alert(3 + 3)     We are using nodejs, not browser*/
+
+let name = "Hemendra"
+let age = 18
+let isLogin = false
+
+console.log(typeof null); // Object
+console.log(typeof undefined); // Object
