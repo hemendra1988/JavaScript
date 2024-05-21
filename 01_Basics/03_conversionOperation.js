c48e3091989c1d67cb168f2d5007bc61ed4446e4
hemendra1988 <hemendravermaiiet@gmail.com>
2024-05-21 22:12:21 +0000

    updates

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,6 +1,20 @@
 let score = "33"
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valInNum = Number(score)
 console.log(typeof valInNum);
+
+// ****************** Operations ********************
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
