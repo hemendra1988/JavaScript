e317c5d194da44260107afc1202983fea8ceb1cc
hemendra1988 <hemendravermaiiet@gmail.com>
2024-05-21 21:09:44 +0000

    Updates

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,8 +1,24 @@
 const accountId = 123456
 let accountEmail = "hemendra@gmail.com"
 var accountPwd = "1234"
-accountCity = "Bareilly"
+let accountCity = "Bareilly"
+let accountState
 
-accountId = 2
+// accountId = 2 // Not Allowed
+
+accountEmail = "HV@gmail.com"
+accountPwd = "1111"
+accountCity = "Noida"
+
+if (typeof accountState == "undefined"){
+    console.log("True");
+}else console.log("False");
 
 console.log(accountId);
+
+/*
+Prefer not to use "var"
+bacause of issue in block scope and functional scope 
+*/
+
+console.table([accountEmail, accountId, accountPwd, accountCity, accountState])
