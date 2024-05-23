906eaf4de973e706602f3cde43f7e5d569f745ec
hemendra1988 <hemendravermaiiet@gmail.com>
2024-05-23 14:39:57 +0000

    updates

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,5 @@
+let name = "Hemendra Verma"
+let age = 30
+let address = "Nottingham, UK"
+
+console.log(`Hi, I am ${name} of age ${age} is living in ${address}`);
---
+++ 01_Basics/06_numbers.js
@@ -0,0 +1,13 @@
+let count = 4007783474702
+
+// console.log(count.toFixed(2));
+// console.log(count.toLocaleString('en-IN'));
+// console.log(count.toLocaleString());
+// console.log(Number.EPSILON);
+
+/* ************ MATHS ***********/
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min);
